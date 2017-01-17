6ac25c605955af86b5b32d15034085aa264a8d64
franblas <francois_blas@hotmail.com>
2017-01-17 14:22:31 +0100

    html templates instead of ejs extensions

--- backend/servers/express-webrouter/app/index.js
+++ backend/servers/express-webrouter/app/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const path = require('path')
+const ejs = require('ejs')
 
 const app = express()
-app.set('view engine', 'ejs')
+app.set('view engine', 'html')
+app.engine('html', ejs.renderFile)
 app.use(express.static(path.join(__dirname, 'static')))
 
 const {
