6c0440b2237cc2a6ed364dc81b8e8a5980b77c19
franblas <francois_blas@hotmail.com>
2017-01-17 18:05:57 +0100

    choose to delete heroku app or not

--- backend/servers/express-webrouter/scripts/_p_heroku_build.sh
+++ backend/servers/express-webrouter/scripts/_p_heroku_build.sh
@@ -4,4 +4,13 @@ if [[ ! $(heroku apps:info -a $[run.subDomain]) ]]; then
   heroku config:set --app $[run.subDomain] TYPE=$[type.name]
 else
   echo "$[run.subDomain] has been already created"
+  echo "Do you wish to delete this app (y/n)?"
+  read answer
+  if echo "$answer" | grep -iq "^y" ;then
+    heroku apps:destroy $[run.subDomain] --confirm $[run.subDomain];
+    heroku create --app $[run.subDomain] --buildpack heroku/python --remote $[type.name];
+    heroku config:set --app $[run.subDomain] TYPE=$[type.name];
+  else
+    echo No
+  fi
 fi
