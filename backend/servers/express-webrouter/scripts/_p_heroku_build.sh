e84237fa162cee9f32a0f33701e1384899d53344
Ledoux <erwan.ledoux@snips.net>
2017-01-23 13:47:16 +0100

    fix syntax in build

--- backend/servers/express-webrouter/scripts/_p_heroku_build.sh
+++ backend/servers/express-webrouter/scripts/_p_heroku_build.sh
@@ -8,11 +8,16 @@ else
     heroku git:remote --app $[run.subDomain] --remote $[type.name]
   else
     echo ".git has been already initiated"
+    if [[ "$(git remote | grep $[type.name])" == "$[type.name]" ]]; then
+      echo "remote has been already set"
+    else
+      heroku git:remote --app $[run.subDomain] --remote $[type.name]
+    fi
   fi
   echo "$[run.subDomain] has been already created"
   echo "Do you wish to delete this app (y/n)?"
   read answer
-  if echo "$answer" | grep -iq "^y" ;then
+  if echo "$answer" | grep -iq "^y"; then
     heroku apps:destroy $[run.subDomain] --confirm $[run.subDomain];
     git init
     heroku create --app $[run.subDomain] --buildpack heroku/nodejs --remote $[type.name];
