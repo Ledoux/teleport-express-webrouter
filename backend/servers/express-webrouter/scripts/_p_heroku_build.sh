9ad72e73c92269a4b2b1b60cd773d35acaba1be3
Ledoux <erwan.ledoux@snips.net>
2017-01-23 11:02:11 +0100

    renamed by type app

--- backend/servers/express-webrouter/scripts/_p_heroku_build.sh
+++ backend/servers/express-webrouter/scripts/_p_heroku_build.sh
@@ -1,21 +1,21 @@
-if [[ ! $(heroku apps:info -a $[backend.dashSiteName]) ]]; then
+if [[ ! $(heroku apps:info -a $[run.subDomain]) ]]; then
   git init
-  heroku create --app $[backend.dashSiteName] --buildpack heroku/nodejs --remote $[type.name]
-  heroku config:set --app $[backend.dashSiteName] TYPE=$[type.name]
+  heroku create --app $[run.subDomain] --buildpack heroku/nodejs --remote $[type.name]
+  heroku config:set --app $[run.subDomain] TYPE=$[type.name]
 else
   if [[ ! -d ".git" ]]; then
-    heroku git:remote --app $[backend.dashSiteName] --remote $[type.name]
+    heroku git:remote --app $[run.subDomain] --remote $[type.name]
   else
     echo ".git has been already initiated"
   fi
-  echo "$[backend.dashSiteName] has been already created"
+  echo "$[run.subDomain] has been already created"
   echo "Do you wish to delete this app (y/n)?"
   read answer
   if echo "$answer" | grep -iq "^y" ;then
+    heroku apps:destroy $[run.subDomain] --confirm $[run.subDomain];
     git init
-    heroku apps:destroy $[backend.dashSiteName] --confirm $[backend.dashSiteName];
-    heroku create --app $[backend.dashSiteName] --buildpack heroku/nodejs --remote $[type.name];
-    heroku config:set --app $[backend.dashSiteName] TYPE=$[type.name];
+    heroku create --app $[run.subDomain] --buildpack heroku/nodejs --remote $[type.name];
+    heroku config:set --app $[run.subDomain] TYPE=$[type.name];
   else
     echo No
   fi
