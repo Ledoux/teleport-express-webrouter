19d60cca4430638eaaddca915e82577229b30b69
erwanledoux <lawanledoux@gmail.com>
2017-06-08 18:49:45 +0200

    refactor backend

--- backend/servers/express-webrouter/app/index.js
+++ backend/servers/express-webrouter/app/index.js
@@ -1,9 +1,40 @@
+require('babel-polyfill')
 const express = require('express')
+const http = require('http')
 
-const app = express()
+const { IS_LOCALHOST } = require('./lib/config')
+const { useRender } = require('./lib/render')
 
-require('./lib/set').setWithApp(app)
-require('./lib/render').renderWithApp(app)
-require('./lib/routes').routesWithApp(app)
+const { SITE_NAME } = process.env
 
-module.exports.app = app
+// in localhost condition we need to import
+// the secret values from a secret script
+if (IS_LOCALHOST) {
+  const env = require('node-env-file')
+  const type = process.env.TYPE || 'development'
+  env(`${__dirname}/../scripts/${type}_localhost_secret.sh`)
+}
+
+function getSetup() {
+  return new Promise((resolve, reject) => {
+    const app = express()
+    app.set('port', (process.env.PORT || 5000))
+    const helmet = require('helmet')
+    app.use(helmet({
+      // NOTE: site stops working in Chrome with this option turned on
+      noSniff: false
+    }))
+    // it is important to put all the apis uses before this useRender
+    useRender(app, {
+      extraContext: {
+        SITE_NAME
+      }
+    })
+    const server = http.Server(app)
+    resolve({ app,
+      server
+    })
+  })
+}
+
+module.exports = getSetup
